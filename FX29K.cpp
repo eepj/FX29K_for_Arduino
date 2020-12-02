088bc4b204c52f93f79985a620dfeb2a9d883f5a
eepj <62833867+eepj@users.noreply.github.com>
2020-12-02 09:43:17 +0800

    Update method names and declaration

--- FX29K.cpp
+++ FX29K.cpp
@@ -5,7 +5,6 @@ FX29::FX29(uint8_t addr, uint8_t range, TwoWire* i2cPtr) {
   _i2cAddr = addr;
   _range = range;
   _i2cPtr = i2cPtr;
-  Wire.begin(_i2cAddr);
 }
 
 FX29::FX29(void) {
@@ -14,6 +13,10 @@ FX29::FX29(void) {
 
 FX29::~FX29(void) {
 
+}
+
+void FX29::begin(void){
+    Wire.begin(_i2cAddr);
 }
 
 void FX29::requestMeasurement(void) {
@@ -33,10 +36,10 @@ float FX29::getPounds(void){
 }
 
 float FX29::getKilograms(void){
-  return getWeightLbf() / 2.205; 
+  return getPounds() / 2.205; 
 }
 
-void write(TwoWire* i2cPtr, uint8_t i2cAddr, uint8_t* arr, uint8_t byteCount) {
+void FX29:: write(TwoWire* i2cPtr, uint8_t i2cAddr, uint8_t* arr, uint8_t byteCount) {
   i2cPtr->beginTransmission(i2cAddr);
   for (uint8_t i = 0; i < byteCount; i++) {
     i2cPtr->write(*(arr + i));
@@ -44,7 +47,7 @@ void write(TwoWire* i2cPtr, uint8_t i2cAddr, uint8_t* arr, uint8_t byteCount) {
   i2cPtr->endTransmission();
 }
 
-void read(TwoWire* i2cPtr, uint8_t i2cAddr, uint8_t* arr, uint8_t byteCount) {
+void FX29::read(TwoWire* i2cPtr, uint8_t i2cAddr, uint8_t* arr, uint8_t byteCount) {
   i2cPtr->requestFrom(i2cAddr, byteCount);
   for (uint8_t i = 0; i < byteCount; i++) {
     *arr = i2cPtr->read();
