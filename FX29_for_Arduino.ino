f8f6a460816673a424f00ad452dc184cef9d651b
eepj <62833867+eepj@users.noreply.github.com>
2020-12-02 09:43:43 +0800

    Add testing sketch

--- FX29_for_Arduino.ino
+++ FX29_for_Arduino.ino
@@ -1,9 +1,19 @@
 #include "FX29K.h"
 
-void setup(){
+FX29 scale(FX29K0_I2C_ADDR, 10, &Wire);
 
+void setup(){
+  Serial.begin(115200);
+  scale.begin();
+  Serial.println("Kilograms\tPounds");
+  Serial.println("-------------------------------");
 }
 
 void loop(){
-  
+  float kg = scale.getKilograms();
+  float lb = scale.getPounds();
+  Serial.print(kg);
+  Serial.print("\t");
+  Serial.println(lb);
+  delay(1000);
 }
