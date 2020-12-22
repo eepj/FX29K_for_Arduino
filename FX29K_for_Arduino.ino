c30def53f7120327aeb4d54ebbb245f0ac6d28b9
Pan <62833867+eepj@users.noreply.github.com>
2020-12-22 17:42:36 +0800

    Update FX29K_for_Arduino.ino

--- FX29K_for_Arduino.ino
+++ FX29K_for_Arduino.ino
@@ -1,8 +1,14 @@
 #include "FX29K.h"
-
+#define MUX_ADDR 0x70
 FX29K scale(FX29K0, 0010, &Wire);
 
-void setup(){
+void setup() {
+  Wire.begin();
+  Wire.begin(MUX_ADDR);
+  Wire.beginTransmission(MUX_ADDR);
+  Wire.write(1 << 7);
+  Wire.endTransmission();
+  delay(100);
   Serial.begin(115200);
   scale.begin();
   scale.tare(1000);
@@ -10,7 +16,7 @@ void setup(){
   Serial.println("--------------------");
 }
 
-void loop(){
+void loop() {
   uint16_t raw = scale.getRawBridgeData();
   float g = scale.getGrams(1000);
   float lb = scale.getPounds(1000);
